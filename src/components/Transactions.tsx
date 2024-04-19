389cff238aedbd85878463c857a0f7e8897e4052
khoi <khoido.kad@gmail.com>
2024-04-18 19:06:52 -0500

    added transactions list

--- src/components/Transactions.tsx
+++ src/components/Transactions.tsx
@@ -1,21 +1,84 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
 
 export default function Transactions() {
-    
-    const transactionIds = [{
-        transactionName: 'Groceries',
-        transactionDate: '4/17/2024',
-        transactionTime: '10:24',
-        transactionAmount: 200
-    }]
 
-    return (
+  const transactionIds = [
+    {
+      id: 1,
+      transactionName: 'Food',
+      transactionDate: '4/17/2024',
+      transactionTime: '10:24',
+      transactionAmount: 200,
+      Image: 'https://picsum.photos/200/300',
+      subscriptionsImage: '',
+      billsImage: '',
+      miscellaneousImage: '',
+    },
+    {
+      id: 2,
+      transactionName: 'Food',
+      transactionDate: '4/17/2024',
+      transactionTime: '10:24',
+      transactionAmount: 200,
+      Image: 'https://picsum.photos/200/300',
+      subscriptionsImage: '',
+      billsImage: '',
+      miscellaneousImage: '',
+    },
+    {
+      id: 3,
+      transactionName: 'Food',
+      transactionDate: '4/17/2024',
+      transactionTime: '10:24',
+      transactionAmount: 200,
+      Image: 'https://picsum.photos/200/300',
+      subscriptionsImage: '',
+      billsImage: '',
+      miscellaneousImage: '',
+    },
+    {
+      id: 4,
+      transactionName: 'Food',
+      transactionDate: '4/17/2024',
+      transactionTime: '10:24',
+      transactionAmount: 200,
+      Image: 'https://picsum.photos/200/300',
+      subscriptionsImage: '',
+      billsImage: '',
+      miscellaneousImage: '',
+    }
+  ]
+
+  return (
     //Use flashlist here
     <View>
-      <Text>January</Text>
+      {transactionIds.map((information) => (
+        <>
+          <View key={information.id} style={{ flexDirection: 'row', justifyContent: 'space-between', margin: 10 }}>
+            <View style={{ flexDirection: 'row' }}>
+              <Image
+                style={styles.smallImage}
+                source={{ uri: information.Image }}
+              />
+              <View>
+                <Text style={{ color: 'white' }}>{information.transactionName}</Text>
+                <Text style={{ color: 'white' }}>{information.transactionDate + information.transactionTime}</Text>
+              </View>
+            </View>
+            <View style={{ justifyContent: 'center' }}>
+              <Text style={{ color: 'white' }}>{information.transactionAmount}</Text>
+            </View>
+          </View>
+        </>
+      ))}
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  smallImage: {
+    width: 40,
+    height: 40
+  }
+})
